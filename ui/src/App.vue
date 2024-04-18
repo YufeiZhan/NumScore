56cdb4c306f42d7c5688f24e54e80fc9d6a8256e
Yufei Zhan <selinazhan@Yufeis-MacBook-Pro.local>
2024-04-18 12:56:23 -0400

    Project Set Up

---
+++ ui/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <b-navbar variant="faded" type="light" class="bg-app">
+        <!-- Logo -->
+        <b-navbar-brand href="#" class="ms-2">🪕 JIANPU</b-navbar-brand>
+
+        <!-- Login / Signup -->
+        <b-navbar-nav class="ms-auto">
+          <b-nav-item> <b-button v-b-modal.login-signup-modal variant="light">Log In/Sign Up</b-button> </b-nav-item>
+        </b-navbar-nav>
+      </b-navbar>
+    </div>
+
+  <router-view />
+</template>
+
