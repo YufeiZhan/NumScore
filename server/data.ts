5b091fb792af60cf3365a36d4fc5115cea449904
Yufei Zhan <selinazhan@Yufeis-MacBook-Pro.local>
2024-04-25 03:13:14 -0400

    Note preview

--- server/data.ts
+++ server/data.ts
@@ -54,7 +54,7 @@ export interface Score {
   timeSignatureBase: number
   tempo: number
   time: Date
-  notes: note[]
+  notes: Note[]
   role?: Role //Optional
 }
 
@@ -65,10 +65,10 @@ export interface ScoreRolePair {
   role: Role
 } 
 
-export interface note {
+export interface Note {
   number: 0 |1 |2 |3 |4 |5 |6 |7
   duration: 1 | 0.5 | 0.25 | 0.125 | 0.0625 | 0.03125 | 0.015625 // 1 - 1/64
-  pitch: 0 | 1 | 2 | 3 | -1 | -2 | -3
+  pitch: 0 | 1 | 2 | 3 | -1 | -2 | -3 // positive means high pitch and negative means low pitch
 }
 
 export const user1 : User = {
@@ -76,11 +76,11 @@ export const user1 : User = {
   name: 'yz858',
   email: 'alice@duke.edu',
   password: '123123',
-  scores: [{scoreId: '1', role: 'Creator'}]
+  scores: [{scoreId: 'Alphabet Song-yz858', role: 'Creator'}]
 }
 
 export const aliceScore1 : Score = {
-  _id: '1',
+  _id: 'Alphabet Song-yz858', // unique id is score title-username
   title: 'Alphabet Song',
   author: 'Alice',
   key: 'C',
@@ -88,29 +88,41 @@ export const aliceScore1 : Score = {
   timeSignatureBase: 4,
   tempo: 120,
   time: new Date(2024, 4, 18, 23, 1, 0),
-  notes: [{number:1, duration:1,pitch:0},{number:1, duration:1,pitch:0},
-          {number:5, duration:1,pitch:0},{number:5, duration:1,pitch:0},
-          {number:6, duration:1,pitch:0},{number:6, duration:1,pitch:0},
-          {number:5, duration:1,pitch:0},
-          {number:4, duration:1,pitch:0},{number:4, duration:1,pitch:0},
-          {number:3, duration:1,pitch:0},{number:3, duration:1,pitch:0},
-          {number:2, duration:1,pitch:0},{number:2, duration:1,pitch:0},
-          {number:1, duration:1,pitch:0},
-          {number:5, duration:1,pitch:0},{number:5, duration:1,pitch:0},
-          {number:4, duration:1,pitch:0},{number:4, duration:1,pitch:0},
-          {number:3, duration:1,pitch:0},{number:3, duration:1,pitch:0},
-          {number:2, duration:1,pitch:0},
-          {number:5, duration:1,pitch:0},{number:5, duration:1,pitch:0},
-          {number:4, duration:1,pitch:0},{number:4, duration:1,pitch:0},
-          {number:3, duration:1,pitch:0},{number:3, duration:1,pitch:0},
-          {number:2, duration:1,pitch:0},
-          {number:1, duration:1,pitch:0},{number:1, duration:1,pitch:0},
-          {number:5, duration:1,pitch:0},{number:5, duration:1,pitch:0},
-          {number:6, duration:1,pitch:0},{number:6, duration:1,pitch:0},
-          {number:5, duration:1,pitch:0},
-          {number:4, duration:1,pitch:0},{number:4, duration:1,pitch:0},
-          {number:3, duration:1,pitch:0},{number:3, duration:1,pitch:0},
-          {number:2, duration:1,pitch:0},{number:2, duration:1,pitch:0},
-          {number:1, duration:1,pitch:0},
-  ],
+  notes: [
+    {"number": 1, "duration": 1, "pitch": 0},
+    {"number": 2, "duration": 1, "pitch": 1},
+    {"number": 3, "duration": 1, "pitch": 2},
+    {"number": 4, "duration": 1, "pitch": 3},
+    {"number": 5, "duration": 1, "pitch": -1},
+    {"number": 6, "duration": 1, "pitch": -2},
+    {"number": 7, "duration": 1, "pitch": -3},
+    {"number": 1, "duration": 0.5, "pitch": 0},
+    {"number": 2, "duration": 0.5, "pitch": 1},
+    {"number": 3, "duration": 0.5, "pitch": 2},
+    {"number": 4, "duration": 0.5, "pitch": 3},
+    {"number": 5, "duration": 0.5, "pitch": -1},
+    {"number": 6, "duration": 0.5, "pitch": -2},
+    {"number": 7, "duration": 0.5, "pitch": -3},
+    {"number": 1, "duration": 0.25, "pitch": 0},
+    {"number": 2, "duration": 0.25, "pitch": 1},
+    {"number": 3, "duration": 0.25, "pitch": 2},
+    {"number": 4, "duration": 0.25, "pitch": 3},
+    {"number": 5, "duration": 0.25, "pitch": -1},
+    {"number": 6, "duration": 0.25, "pitch": -2},
+    {"number": 7, "duration": 0.25, "pitch": -3},
+    {"number": 1, "duration": 0.125, "pitch": 0},
+    {"number": 2, "duration": 0.125, "pitch": 1},
+    {"number": 3, "duration": 0.125, "pitch": 2},
+    {"number": 4, "duration": 0.125, "pitch": 3},
+    {"number": 5, "duration": 0.125, "pitch": -1},
+    {"number": 6, "duration": 0.125, "pitch": -2},
+    {"number": 7, "duration": 0.125, "pitch": -3},
+    {"number": 1, "duration": 0.0625, "pitch": 0},
+    {"number": 2, "duration": 0.0625, "pitch": 1},
+    {"number": 3, "duration": 0.0625, "pitch": 2},
+    {"number": 4, "duration": 0.0625, "pitch": 3},
+    {"number": 5, "duration": 0.0625, "pitch": -1},
+    {"number": 6, "duration": 0.0625, "pitch": -2},
+    {"number": 7, "duration": 0.0625, "pitch": -3}
+  ]  
 }
